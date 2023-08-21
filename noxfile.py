4da4cbcd2f60884a241fae1030f41b3a81d47189
florianvazelle <ponythugflorian@gmail.com>
2023-08-21 21:27:33 +0200

    fix(ci): clean workflows files

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,13 @@ import nox
 
 
 @nox.session
+def lint(session: nox.Session) -> None:
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run("pre-commit", "run", "-a")
+
+
+@nox.session
 def test(session: nox.Session) -> None:
     session.install("poetry")
     session.run("poetry", "install")
